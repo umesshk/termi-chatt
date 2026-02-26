c57dc63d53bc7d861f17960a43a14c378b78a364
umesshk <umeshkumar01439@gmail.com>
2026-02-26 18:09:15 +0530

    http server

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/umeshhk/termi-chatt
 
 go 1.24.0
 
-require github.com/gorilla/websocket v1.5.3
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server/server.go
+++ server/server.go
@@ -1,1 +1,24 @@
+package server 
 
+import (
+		"log"
+		"net/http"
+)
+func StartServer(){ 
+	
+	PORT := ":8080"
+	
+	server := &http.Server{
+		Addr : PORT,
+		Handler: nil, 
+	}
+
+	log.Printf("Server Running on  %v", PORT)
+	
+	err := server.ListenAndServe()
+
+	if err!=nil {
+		log.Fatal(err)
+	}
+
+}
