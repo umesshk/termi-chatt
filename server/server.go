79e34c185e263d503b53e5116662f9dddf6498f8
umesshk <umeshkumar01439@gmail.com>
2026-02-26 18:59:44 +0530

    ping pong websocket server

--- server/server.go
+++ server/server.go
@@ -3,16 +3,58 @@ package server
 import (
 		"log"
 		"net/http"
+		"github.com/gorilla/websocket"
 )
 
-func HomeHandler(w http.ResponseWriter,r *http.Request){
-	bytes, err := w.Write([]byte("Hello from Server "))
+var upgrader = websocket.Upgrader{
+	ReadBufferSize  : 1024,
+	WriteBufferSize : 1024,
+}
 
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	log.Printf("%v Bytes of Data written Succesfully ",bytes)
+
+func HomeHandler(w http.ResponseWriter,r *http.Request){
+		conn, err := upgrader.Upgrade(w,r,nil)
+		
+		defer conn.Close()
+
+		if err != nil {
+				log.Fatal(err)
+				return 
+		}
+		
+		 for {
+				messageType , p, err := conn.ReadMessage()
+				
+				if err != nil {
+					log.Fatal(err)
+					return 
+				}
+
+				ClientMessage := string(p)
+
+			log.Printf("%v",ClientMessage)
+
+
+			if(ClientMessage=="pong"){
+				
+			if err := conn.WriteMessage(messageType,[]byte("ping")); err != nil{
+						log.Fatal(err)
+						return 
+			}
+
+		}else {
+				if err := conn.WriteMessage(messageType,[]byte("pong")); err != nil{
+						log.Fatal(err)
+						return 
+			}
+
+
+
+		}
+
+	  }	
+
+			
 } 
 
 
@@ -23,7 +65,7 @@ func StartServer(){
 
 	log.Printf("Starting Server on  %v\n", PORT)
 	
-	http.HandleFunc("/hello",HomeHandler)
+	http.HandleFunc("/ws",HomeHandler)
 	err := http.ListenAndServe(PORT,nil)
 
 	if err!=nil {
