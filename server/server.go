fa881ef9f51f4673ddc137f47d2c07f2779f5ccd
umesshk <umeshkumar01439@gmail.com>
2026-02-26 19:02:40 +0530

    some changes

--- server/server.go
+++ server/server.go
@@ -1,77 +1,68 @@
-package server 
+package server
 
 import (
-		"log"
-		"net/http"
-		"github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize  : 1024,
-	WriteBufferSize : 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 
-func HomeHandler(w http.ResponseWriter,r *http.Request){
-		conn, err := upgrader.Upgrade(w,r,nil)
-		
-		defer conn.Close()
+	defer conn.Close()
 
-		if err != nil {
-				log.Fatal(err)
-				return 
-		}
-		
-		 for {
-				messageType , p, err := conn.ReadMessage()
-				
-				if err != nil {
-					log.Fatal(err)
-					return 
-				}
-
-				ClientMessage := string(p)
-
-			log.Printf("%v",ClientMessage)
-
-
-			if(ClientMessage=="pong"){
-				
-			if err := conn.WriteMessage(messageType,[]byte("ping")); err != nil{
-						log.Fatal(err)
-						return 
-			}
-
-		}else {
-				if err := conn.WriteMessage(messageType,[]byte("pong")); err != nil{
-						log.Fatal(err)
-						return 
-			}
-
-
-
-		}
-
-	  }	
-
-			
-} 
-
-
-func StartServer(){ 
-	
-	PORT := ":8080"
-	
-
-	log.Printf("Starting Server on  %v\n", PORT)
-	
-	http.HandleFunc("/ws",HomeHandler)
-	err := http.ListenAndServe(PORT,nil)
-
-	if err!=nil {
+	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
+	for {
+		messageType, p, err := conn.ReadMessage()
 
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		ClientMessage := string(p)
+
+		log.Printf("%v", ClientMessage)
+
+		if ClientMessage == "pong" {
+
+			if err := conn.WriteMessage(messageType, []byte("ping")); err != nil {
+				log.Println(err)
+				return
+			}
+
+		} else {
+			if err := conn.WriteMessage(messageType, []byte("pong")); err != nil {
+				log.Println(err)
+				return
+			}
+
+		}
+
+	}
+
+}
+
+func StartServer() {
+
+	PORT := ":8080"
+
+	log.Printf("Starting Server on  %v\n", PORT)
+
+	http.HandleFunc("/ws", HomeHandler)
+	err := http.ListenAndServe(PORT, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
