f64f3f3e4bd9ea46ecada34571bc61b44a902dd6
umesshk <umeshkumar01439@gmail.com>
2026-03-04 00:10:03 +0530

    join and create room logic with struct

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"github.com/gorilla/websocket"
 	"log"
+	"encoding/json"
 	"net/http"
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 var upgrader = websocket.Upgrader{
@@ -20,9 +19,9 @@ var room_map = make(map[int][]*websocket.Conn)
 var conn_map = make(map[*websocket.Conn]int)
 
 type UserMessage struct {
- Msgtype 		string `json:"type"` 
- Message		string  `json:"message"`
- RoomId   	int			`json:"roomid"`
+ Msgtype 		string  `json:"type"` 
+ Message		string  `json:"message,omitempty"` 
+ RoomId   	int			`json:"roomId,omitempty"`
 }
 
 func MainHanlder(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +31,7 @@ func MainHanlder(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Fatal(err)
-		return
+	 return 	
 	}
 
 	for {
@@ -40,62 +39,67 @@ func MainHanlder(w http.ResponseWriter, r *http.Request) {
 
 		if err != nil {
 			log.Fatal(err)
-			return
+			continue
 		}
 
-		ClientMessage := strings.TrimSpace(string(p))
+	var ClientMessage UserMessage 
+ 
+	if err := json.Unmarshal(p,&ClientMessage); err != nil{
+			log.Println(err)
+			continue	
+	} 
+		switch ClientMessage.Msgtype {
+		case "create" :
+				roomId++
+				room_map[roomId] = append(room_map[roomId], conn)	
+	    	conn_map[conn] = roomId	
 
+				message := fmt.Sprintf("Room Created with Id : %d",roomId)
 
-		if ClientMessage == "create" {
-			roomId++
-			room_map[roomId] = append(room_map[roomId], conn)	
-	    conn_map[conn] = roomId	
+				if err := conn.WriteMessage(messageType,[]byte(message)); err!=nil {
+					log.Println(err)
+					continue 
+				}	 
 
-			message := fmt.Sprintf("Room Created with Id : %d",roomId)
-
-			if err := conn.WriteMessage(messageType,[]byte(message)); err!=nil {
-				log.Fatal(err)
-				return 
-			} 
-
-			log.Printf("User %v Created room %v \n" , conn.RemoteAddr(), roomId)
-
-		} 
-		if ClientMessage == "join" {
-
-			roomIdString:= ClientMessage
-
-			if err != nil{
-				log.Fatal(err)
-				return 
-			}
-			room_id,err := strconv.Atoi(roomIdString)
+				log.Printf("User %v Created room %v \n" , conn.RemoteAddr(), roomId)
+	
+			case "join" :  
 			
-			if err != nil{
-				log.Fatal(err)
-				return 
-			}
-			
-			room_Conn , ok := room_map[room_id]	
-				if !ok {
-					if err := conn.WriteMessage(messageType,[]byte("Room doesn't exist")); err != nil {
-						log.Fatal(err)
-						return 
-					}
-				}else {
-					if err := conn.WriteMessage(messageType,[]byte("Room Joined ")); err != nil {
+				room_id := ClientMessage.RoomId
+
+				if err != nil{
 					log.Fatal(err)
 					return 
-					}
-				}
-			}
+				}		
+			
+				_, ok := room_map[room_id]	
+					if !ok {
+						if err := conn.WriteMessage(messageType,[]byte("Room doesn't exist")); err != nil {
+							log.Fatal(err)
+							continue
+						}
 
+					}else {
+						room_map[room_id] = append(room_map[room_id], conn)
+	    			conn_map[conn] = room_id
+						if err := conn.WriteMessage(messageType,[]byte("Room Joined ")); err != nil {
+						log.Fatal(err)
+				  	continue 	
+						}
+						log.Printf("User Joined Room %v\n", room_id)
+					}
+	    case "message": 
+			
+			default : 
+				if err := conn.WriteMessage(messageType,[]byte("Invalid Input ")) ; err != nil {
+					log.Println(err)
+				}
 			
 
 		}
 
 	}
-
+}
 
 func main() {
 
